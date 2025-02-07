5db085f04823e48d1be7db97a664663e3c6cda80
Kayla Blosser <kayla.blosser@proton.me>
2025-02-07 10:02:06 -0500

    Create project scaffold

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import cssnano from 'cssnano';
+import discardUnused from 'postcss-discard-unused';
+import colormin from 'postcss-colormin';
+import discardDuplicates from 'postcss-discard-duplicates';
+
+export default defineConfig({
+    css: {
+        postcss: {
+            plugins: [
+                cssnano({
+                    plugins: [
+                        discardUnused,
+                        colormin,
+                        discardDuplicates
+                    ]
+                })
+            ]
+        }
+    },
+    plugins: [
+        tailwindcss(),
+    ],
+})
