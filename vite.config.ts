9017e4bee4aed5e86895b9061f0a25fa0fff60be
Kayla Blosser <kayla.blosser@proton.me>
2025-02-08 16:47:24 -0500

    Almost done switching to lightning

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import lightningcss from 'vite-plugin-lightningcss'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 
 export default defineConfig({
     plugins: [
@@ -12,13 +15,20 @@ export default defineConfig({
                     dest: 'assets'
                 }
             ]
-        })
+        }),
+        lightningcss({
+            browserslist: "last 2 versions",
+        }),
     ],
     base: '/',
     build: {
         cssMinify: 'lightningcss',
+        cssCodeSplit: true,
     },
     css: {
-        
+        transformer: 'lightningcss',
+        lightningcss: {
+            targets: browserslistToTargets(browserslist('>= 0.25%'))
+        }
     }
 })
