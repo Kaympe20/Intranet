79f98195cdb122dedecaaa59863530209e79df7c
Kayla Blosser <kayla.blosser@proton.me>
2025-02-08 16:42:06 -0500

    Switch postcss to lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import cssnano from 'cssnano';
-import discardUnused from 'postcss-discard-unused';
-import colormin from 'postcss-colormin';
-import discardDuplicates from 'postcss-discard-duplicates';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-    css: {
-        postcss: {
-            plugins: [
-                cssnano({
-                    plugins: [
-                        discardUnused,
-                        colormin,
-                        discardDuplicates
-                    ]
-                })
-            ]
-        }
-    },
     plugins: [
         tailwindcss(),
         viteStaticCopy({
@@ -32,4 +15,10 @@ export default defineConfig({
         })
     ],
     base: '/',
+    build: {
+        cssMinify: 'lightningcss',
+    },
+    css: {
+        
+    }
 })
